3b650c5e52e654b8e524483a9aa8dd8ee22a2803
DebopriyoRoy <debopriyo.ca@gmail.com>
2026-03-14 12:41:52 -0230

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "ec2_instance" {
+  source  = "./ec2"
+  }
+module "vpc_inst" {
+  source  = "./vpc"
+  }
+
