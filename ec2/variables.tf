11e9f6c7ee6bcd2f639e7eee48fbf700357d26bb
DebopriyoRoy <debopriyo.ca@gmail.com>
2026-03-14 12:43:12 -0230

    Add files via upload

---
+++ ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  default = "t3.large"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  default = "ami-0f3caa1cf4417e51b"
+}
+
+
